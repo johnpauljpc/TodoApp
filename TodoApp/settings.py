19e450019960dfc0dfcf5ae1297ca07240c98ab3
Gabriel Johnpaul Chigozie <64210565+johnpauljpc@users.noreply.github.com>
2022-11-30 12:43:49 +0100

    Update settings.py

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2xjh6y$f_dxx@rz^p%z58@pm*4y)il!-smm$77iy_1$w^eczo)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
